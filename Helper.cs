271982f6578bf0703d66f28ad9912ddcbaf46d40
bogdanalban <bogdanvalban@outlook.com>
2022-06-08 14:23:27 +0300

    commit

---
+++ Helper.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace NotepadPlusPlusDoi
+{
+    public class Helper
+    {
+       // public string remove
+
+        public void createNewFile(ref List<FileElement> listOfFiles, ref TabControl tabcontrol)
+        {
+            TabItem ti = new TabItem();
+            TextBox tb = new TextBox();
+            tb.TextWrapping = TextWrapping.Wrap;
+            tb.AcceptsReturn = true;
+            ti.Name = "FileNr" + tabcontrol.Items.Count.ToString();
+            ti.Header = "FileNr" + tabcontrol.Items.Count.ToString();
+            ti.Foreground = new SolidColorBrush(Color.FromArgb(0xFF, 0xFF, 0, 0));
+            ti.Background = new SolidColorBrush(Color.FromArgb(0xFF, 1, 1, 1));
+            
+            ti.Content = tb;
+            tabcontrol.Items.Add(ti);
+            FileElement newFile = new FileElement();
+            newFile.fileName = ti.Name;
+            listOfFiles.Add(newFile);
+            tabcontrol.SelectedItem = ti;
+        }
+
+
+        public void save(ref List<FileElement> listOfFiles, int index, ref TabControl tabcontrol)
+        {
+            TabItem selectedItem = (TabItem)tabcontrol.SelectedItem;
+            TextBox tb = (TextBox)selectedItem.Content;
+            if (listOfFiles[index].filePath != null)
+            {
+                File.WriteAllText(listOfFiles[index].filePath, tb.Text.ToString());
+                selectedItem.Foreground = new SolidColorBrush(Color.FromRgb(0, 255, 0));
+            }
+            else
+            {
+                saveAsFile(ref listOfFiles, index, ref tabcontrol);
+            }
+        }
+
+
+
+        public void saveAsFile(ref List<FileElement> listOfFiles, int index, ref TabControl tabcontrol)
+        {
+
+            TabItem selectedItem = (TabItem)tabcontrol.SelectedItem;
+            if (selectedItem != null)
+            {
+                TextBox tb = (TextBox)selectedItem.Content;
+                SaveFileDialog dialog = new SaveFileDialog();
+                dialog.FileName = listOfFiles[index].fileName;
+                dialog.DefaultExt = ".txt";
+                dialog.Filter = "Text Documents (.txt) |*.txt";
+                dialog.ShowDialog();
+                File.WriteAllText(dialog.FileName, tb.Text.ToString());
+                listOfFiles[index].filePath = dialog.FileName;
+                selectedItem.Foreground = new SolidColorBrush(Color.FromArgb(255, 0, 255, 0));
+                selectedItem.Header = dialog.SafeFileName;
+                listOfFiles[index].fileName = dialog.SafeFileName;
+
+            }
+            else
+                MessageBox.Show("Please select a file!");
+        }
+
+        public void openFile(ref List<FileElement> listOfFiles, ref TabControl tabcontrol)
+        {
+            TabItem ti = new TabItem();
+            TextBox tb = new TextBox();
+            tb.AcceptsReturn = true;
+            FileElement tabList = new FileElement();
+            tb.TextWrapping = TextWrapping.Wrap;
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text File (*.txt) | *.txt";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                using (StreamReader streamReader = new StreamReader(openFileDialog.FileName))
+                {
+                    tabList.filePath = openFileDialog.FileName;
+                    Task<String> text = streamReader.ReadToEndAsync();
+                    tb.Text = text.Result;
+                    ti.Content = tb;
+                    ti.Header = openFileDialog.SafeFileName;
+                    ti.Foreground = new SolidColorBrush(Color.FromRgb(0, 255, 0));
+                    ti.Background = new SolidColorBrush(Color.FromArgb(0xFF, 1, 1, 1));
+
+                    tabcontrol.Items.Add(ti);
+                    tabList.fileName = openFileDialog.SafeFileName;
+                    listOfFiles.Add(tabList);
+                    tabcontrol.SelectedItem = ti;
+                }
+            }
+        }
+
+        public void close(ref List<FileElement> listOfFiles, int index , ref TabControl tabcontrol)
+        {
+            TabItem selectedItem = (TabItem)tabcontrol.SelectedItem;
+            tabcontrol.Items.Remove(selectedItem);
+            listOfFiles.Remove(listOfFiles[index]);
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace NotepadPlusPlusDoi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        List<FileElement> listOfFiles = new List<FileElement>();
+        Helper helper = new Helper();
+
+        private void NewFile(object sender, RoutedEventArgs e)
+        {
+            helper.createNewFile(ref listOfFiles, ref TabControl1);
+            ((TabControl1.SelectedItem as TabItem).Content as TextBox).TextChanged += TextArea_TextChanged;
+        }
+
+        int indexOfFileInListOfFiles(String Name)
+        {
+            for (int i = 0; i < listOfFiles.Count; i++) if  (listOfFiles[i].fileName == Name) return i; 
+            return -1;
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            TabItem selectedItem = (TabItem)TabControl1.SelectedItem;
+            helper.save(ref listOfFiles, indexOfFileInListOfFiles(selectedItem.Header.ToString()), ref TabControl1);
+        }
+
+        private void SaveAs(object sender, RoutedEventArgs e)
+        {
+
+            TabItem selectedItem = (TabItem)TabControl1.SelectedItem;
+            helper.saveAsFile(ref listOfFiles, indexOfFileInListOfFiles(selectedItem.Header.ToString()), ref TabControl1);
+        }
+
+        private void Open(object sender, RoutedEventArgs e)
+        {
+            helper.openFile(ref listOfFiles, ref TabControl1);
+            ((TabControl1.SelectedItem as TabItem).Content as TextBox).TextChanged += TextArea_TextChanged;
+        }
+
+        private void Close(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Do you really want to close this file?", "Message", MessageBoxButton.YesNo);
+            switch (result)
+            {
+
+                case MessageBoxResult.Yes:
+                    TabItem selectedItem = (TabItem)TabControl1.SelectedItem;
+                    helper.saveAsFile(ref listOfFiles, indexOfFileInListOfFiles(selectedItem.Header.ToString()), ref TabControl1);
+                    helper.close(ref listOfFiles, indexOfFileInListOfFiles(selectedItem.Header.ToString()), ref TabControl1);
+                 
+                    break;
+                case MessageBoxResult.No:
+                    break;
+
+            }
+
+        }
+
+        private void TextArea_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TabItem selectedItem = (TabItem)TabControl1.SelectedItem;
+            selectedItem.Foreground = new SolidColorBrush(Color.FromArgb(0xFF, 0xFF, 0, 0));
+        }
+
+        private void About(object sender, RoutedEventArgs e)
+        {
+            About about = new About();
+            about.Show();
+        }
+
+
+
+        
+    }
+
+}
